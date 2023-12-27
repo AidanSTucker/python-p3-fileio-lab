f4944e54318c5b1e49047557bcd6727ba4b47728
elpatron <aidanseantucker@gmail.com>
2023-12-27 11:55:39 -0500

    passing 2/3 tests

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    file_name = Path(file_name).with_suffix('.txt')  # Convert to Path object and add .txt extension
+    with open(file_name, "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name = Path(file_name).with_suffix('.txt')  # Convert to Path object and add .txt extension
+    with open(file_name, "a") as file:
+        file.write("\n" + append_content)
 
 def read_file(file_name):
-    pass
+    file_name = Path(file_name).with_suffix('.txt')  # Convert to Path object and add .txt extension
+    with open(file_name, "r") as file:
+        content = file.read()
+    return content
